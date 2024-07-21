7935c8843d1fca56ddf492072e769945fcb1d154
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-07-21 16:09:39 +0530

    Time: 11 ms (59.06%), Space: 55.2 MB (79.53%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.java
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.java
@@ -1,44 +1,48 @@
 class Solution {
     
-    private void dfs(int node, Map<Integer, List<Integer>> adj, int[] visited,
-                     Stack<Integer> stack, boolean[] hasCycle) {
-        visited[node] = 1; // Mark node as visiting
-
-        for (int neighbor : adj.getOrDefault(node, new ArrayList<>())) {
-            if (visited[neighbor] == 0) {
-                dfs(neighbor, adj, visited, stack, hasCycle);
-            } else if (visited[neighbor] == 1) {
-                hasCycle[0] = true; // Cycle detected
-                return;
-            }
-        }
-
-        visited[node] = 2; // Mark node as visited
-        stack.push(node); // Add node to the stack
-    }
-    
     private List<Integer> topoSort(int[][] edges, int n) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] indegree = new int[n + 1];
         List<Integer> order = new ArrayList<>();
-        int[] visited = new int[n + 1]; // 0: not visited, 1: visiting, 2: visited
-        boolean[] hasCycle = {false};
+        
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
 
         for (int[] edge : edges) {
-            adj.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            indegree[v]++;
         }
 
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+        // Push all integers with in-degree 0 in the queue.
         for (int i = 1; i <= n; i++) {
-            if (visited[i] == 0) {
-                dfs(i, adj, visited, stack, hasCycle);
-                if (hasCycle[0])
-                    return new ArrayList<>(); // no ordering possible
+            if (indegree[i] == 0) {
+                queue.add(i);
+                count++;
             }
         }
 
-        while (!stack.isEmpty()) {
-            order.add(stack.pop());
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            order.add(u);
+            
+            for (int v : adj.get(u)) {
+                indegree[v]--;
+
+                if (indegree[v] == 0) {
+                    queue.add(v);
+                    count++;
+                }
+            }
         }
+
+        if (count != n) 
+            return new ArrayList<>(); // Cycle detected
+
         return order;
     }
     
