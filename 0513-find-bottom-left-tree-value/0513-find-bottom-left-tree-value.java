a0caf4862938aaee8ac55585ab1d8ba7c195c121
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-07-21 23:52:12 +0530

    Time: 0 ms (100.00%), Space: 44 MB (90.29%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,24 +14,26 @@
  * }
  */
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        Queue<TreeNode> que = new LinkedList<>();
-        que.offer(root);
-        
-        int result = 0;
-        while (!que.isEmpty()) {
-            int n = que.size();
-            while (n-- > 0) {
-                TreeNode node = que.poll();
-                result = node.val;
-                if (node.right != null) {
-                    que.offer(node.right);
-                }
-                if (node.left != null) {
-                    que.offer(node.left);
-                }
-            }
+    int maxDepth = -1;
+    int result = 0;
+    public void solve(TreeNode root, int depth) {
+        if (root == null) {
+            return;
         }
+        
+        if (depth > maxDepth) {
+            maxDepth = depth;
+            result = root.val;
+        }
+        
+        solve(root.left, depth+1);
+        solve(root.right, depth+1);
+        
+    }
+    
+    public int findBottomLeftValue(TreeNode root) {
+
+        solve(root, 0);
         return result;
     }
 }
