3f5edbbed5878729f43d9c5be2e24a29cd971f85
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-07-23 21:36:11 +0530

    Time: 5 ms (92.24%), Space: 44.4 MB (59.67%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] count = new int[201];
+
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            count[num + 100]++;
         }
-        
+
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums) {
+            numList.add(num);
+        }
+
         Comparator<Integer> comparator = (a, b) -> {
-            int freqCompare = Integer.compare(map.get(a), map.get(b));
+            int freqCompare = Integer.compare(count[a + 100], count[b + 100]);
             if (freqCompare == 0) {
                 return Integer.compare(b, a);
             } else {
                 return freqCompare;
             }
         };
-        
-        List<Integer> numList = new ArrayList<>();
-        for (int num : nums) {
-            numList.add(num);
-        }
-        
+
         numList.sort(comparator);
-        
+
         for (int i = 0; i < nums.length; i++) {
             nums[i] = numList.get(i);
         }
-        
+
         return nums;
     }
 }
