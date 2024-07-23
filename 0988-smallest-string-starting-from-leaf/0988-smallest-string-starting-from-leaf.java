a42c687ae6c6e355b2a909e86db42d94b8e6179e
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-07-23 23:23:42 +0530

    Time: 2 ms (89.23%), Space: 44.4 MB (77.56%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -14,33 +14,31 @@
  * }
  */
 class Solution {
-    public String smallestFromLeaf(TreeNode root) {
-        Queue<Pair<TreeNode, StringBuilder>> que = new LinkedList<>();
-        StringBuilder result = new StringBuilder();
-        que.offer(new Pair<>(root, new StringBuilder().append((char)(root.val + 'a'))));
-        
-        while (!que.isEmpty()) {
-            Pair<TreeNode, StringBuilder> pair = que.poll();
-            TreeNode node = pair.getKey();
-            StringBuilder curr = pair.getValue();
-            
-            if (node.left == null && node.right == null) {
-                String currStr = curr.toString();
-                if (result.length() == 0 || result.toString().compareTo(currStr) > 0) {
-                    result.setLength(0);
-                    result.append(currStr);
-                }
-            }
-            
-            if (node.left != null) {
-                que.offer(new Pair<>(node.left, new StringBuilder(curr).insert(0, (char)(node.left.val + 'a'))));
-            }
-            
-            if (node.right != null) {
-                que.offer(new Pair<>(node.right, new StringBuilder(curr).insert(0, (char)(node.right.val + 'a'))));
-            }
+    String result;
+    
+    private void solve(TreeNode root, StringBuilder curr) {
+        if (root == null) {
+            return; 
         }
         
-        return result.toString();
+        curr.insert(0, (char)(root.val + 'a'));
+        
+        if (root.left == null && root.right == null) {
+            if (result.equals("") || result.compareTo(curr.toString()) > 0) {
+                result = curr.toString();
+            }
+            
+        }
+        
+        solve(root.left, curr);
+        solve(root.right, curr);
+        
+        curr.deleteCharAt(0);
+    }
+    
+    public String smallestFromLeaf(TreeNode root) {
+        result = "";
+        solve(root, new StringBuilder());
+        return result;
     }
 }
