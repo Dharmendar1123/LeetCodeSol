ad58da48ef1a23a5347275d8104facac344e2132
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-07-30 11:42:41 +0530

    Time: 19 ms (92.48%), Space: 45.9 MB (5.01%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,25 +1,16 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int n = s.length();
+        int countB = 0;
+        int dlt = 0;
 
-        int counta = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) == 'a')
-                counta++;
+        for(char c : s.toCharArray()) {
+            if(c == 'a') {
+                dlt = Math.min(dlt + 1, countB);
+            } else {
+                countB++;
+            }
         }
 
-        int count = Integer.MAX_VALUE;
-        int countb = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == 'a')
-                counta--;
-            count = Math.min(count, countb + counta);
-
-            if (s.charAt(i) == 'b')
-                countb++;
-        }
-
-        return count;
+        return dlt;
     }
 }
