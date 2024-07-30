dfc946b1e5704f87580de20ac0c8cf68d07ffbfa
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-07-30 23:10:37 +0530

    Time: 5 ms (77.39%), Space: 46 MB (35.02%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,44 +1,50 @@
 class Solution {
     
+    private boolean isCycle(int u, Map<Integer, List<Integer>> adj, boolean[] visited, boolean[] inRec, Stack<Integer> st) {
+        visited[u] = true;
+        inRec[u] = true;
+        
+        for (int v : adj.getOrDefault(u, new ArrayList<>())) {
+            if (!visited[v] && isCycle(v, adj, visited, inRec, st)) {
+                return true;
+            }else if (inRec[v] == true) {
+                return true;
+            }
+        }
+        
+        st.push(u);
+        inRec[u] = false;
+        return false;
+    }
+    
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> adj = new HashMap<>();
-        int[] inDegree = new int[numCourses];
         
         for (int[] row : prerequisites) {
             int a = row[0];
             int b = row[1];
             
             adj.computeIfAbsent(b, k -> new ArrayList<>()).add(a);
-            inDegree[a]++;
+        }
+        
+        boolean[] visited = new boolean[numCourses];
+        boolean[] inRec = new boolean[numCourses];
+        
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = 0; i < numCourses; ++i) {
+            if (!visited[i] && isCycle(i, adj, visited, inRec, st)) {
+                return new int[] {};
+            }
         }
         
         int[] result = new int[numCourses];
-        
-        Queue<Integer> que = new LinkedList<>();
-        int count = 0;
-        
-        for (int i = 0; i < numCourses; ++i) {
-            if (inDegree[i] == 0) {
-                que.offer(i);
-                count++;
-            }
-        }
         int i = 0;
-        
-        while (!que.isEmpty()) {
-            int u = que.poll();
-            result[i++] = u;
-            
-            for (int v : adj.getOrDefault(u, new ArrayList<>())) {
-                inDegree[v]--;
-                
-                if (inDegree[v] == 0) {
-                    que.offer(v);
-                    count++;
-                }
-            }
+        while (!st.isEmpty()) {
+            result[i++] = st.pop();
         }
         
-        return count == numCourses ? result : new int[] {};
+        
+        return result;
     }
 }
