a6d302e7478664a601899fd1fcd5e1c039940806
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-04 14:50:58 +0530

    Time: 80 ms (39.64%), Space: 44.6 MB (82.43%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,24 +1,24 @@
 class Solution {
-    private static final int M = (int)1e9 + 7;
-
     public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Integer> temp = new ArrayList<>();
+        int mod = (int)1e9 + 7;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                temp.add(sum);
+        for (int i = 0; i < n; ++i) {
+            pq.offer(new int[] {nums[i], i});
+        }
+        
+        int result = 0;
+        for (int i = 1; i <= right; ++i) {
+            int[] curr = pq.poll();
+            
+            if (i >= left) {
+                result = (result + curr[0]) % mod;
+            }
+            
+            if (curr[1] < n - 1) {
+                pq.offer(new int[] {curr[0] + nums[curr[1] + 1], curr[1] + 1});
             }
         }
-
-        Collections.sort(temp);
-
-        int result = 0;
-        for (int i = left - 1; i < right; i++) {
-            result = (result + temp.get(i)) % M;
-        }
-
         return result;
     }
 }
