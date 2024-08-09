70710eac9f7ce6c9a2ef9f9e642496bf70aaa401
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-09 18:36:39 +0530

    Time: 392 ms (35.80%), Space: 61.7 MB (55.78%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,47 +1,75 @@
 class Solution {
+    
+    static int[] parent;
+    static int[] rank;
+    
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        
+        return parent[x] = find(parent[x]);
+    }
+    
+    static void union(int x, int y) {
+        int parentX = find(x);
+        int parentY = find(y);
+        
+        if (parentX == parentY) {
+            return;
+        }
+        
+        if (rank[parentX] > rank[parentY]) {
+            parent[parentY] = parentX;
+        }else if (rank[parentX] < rank[parentY]) {
+            parent[parentX] = parentY;
+        }else {
+            parent[parentX] = parentY;
+            rank[parentY]++;
+        }
+    }
+    
+    public int kruskal(List<int[]> adj) {
+        int sum = 0;
+        
+        for (int[] temp : adj) {
+            int u = temp[0];
+            int v = temp[1];
+            int wt = temp[2];
+            
+            int parentU = find(u);
+            int parentV = find(v);
+            
+            if (parentU != parentV) {
+                union(u, v);
+                sum += wt;
+            }
+        }
+        
+        return sum;
+    }
+    
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
         
-        List<List<int[]>> adj = new ArrayList<>();
+        parent = new int[n];
+        rank = new int[n];
         for (int i = 0; i < n; ++i) {
-            adj.add(new ArrayList<>());
+            parent[i] = i;
         }
+        
+        List<int[]> adj = new ArrayList<>();
         
         for (int i = 0; i < n; ++i) {
             for (int j = i+1; j < n; ++j) {
                 int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
                 
-                adj.get(i).add(new int[] {j, dist});
-                adj.get(j).add(new int[] {i, dist});
+                adj.add(new int[] {i, j, dist});
             }
         }
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        boolean[] inMST = new boolean[n];
-        pq.offer(new int[] {0, 0});
-        int sum = 0;
+        Collections.sort(adj, (a, b) -> a[2] - b[2]);
         
-        while (!pq.isEmpty()) {
-            int[] top = pq.poll();
-            int wt = top[0];
-            int node = top[1];
-            
-            if (inMST[node] == true) {
-                continue;
-            }
-            
-            inMST[node] = true;
-            sum += wt;
-            
-            for (int[] temp : adj.get(node)) {
-                int nbr = temp[0];
-                int nbrWt = temp[1];
-                
-                if (inMST[nbr] == false) {
-                    pq.offer(new int[] {nbrWt, nbr});
-                }
-            }
-        }
-        return sum;
+        return kruskal(adj);
     }
 }
