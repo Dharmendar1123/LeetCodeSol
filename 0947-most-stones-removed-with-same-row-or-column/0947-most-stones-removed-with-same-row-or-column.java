a98f4baf372b9c05674fd41eef7d2d1b467f1f81
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-11 18:13:41 +0530

    Time: 7 ms (94.27%), Space: 44.6 MB (83.37%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,29 +1,48 @@
 class Solution {
     
-    public void dfs(int index, int[][] stones, boolean[] visited) {
-        visited[index] = true;
+    public int find(int x, int[] parent) {
+        if (parent[x] == -1) {
+            return x;
+        }
         
-        for (int i = 0; i < stones.length; ++i) {
-            int r = stones[index][0];
-            int c = stones[index][1];
-            if (!visited[i] && (stones[i][0] == r || stones[i][1] == c)) {
-                dfs(i, stones, visited);
-            }
+        return parent[x] = find(parent[x], parent);
+    }
+    
+    public void union(int x, int y, int[] parent, int[] rank) {
+        
+        int parentX = find(x, parent);
+        int parentY = find(y, parent);
+        
+        if (parentX == parentY) {
+            return;
+        }
+        
+        if (rank[parentX] > rank[parentY]) {
+            parent[parentY] = parentX;
+        }else if (rank[parentX] < rank[parentY]) {
+            parent[parentX] = parentY;
+        }else {
+            parent[parentX] = parentY;
+            rank[parentY]++;
         }
     }
     
     public int removeStones(int[][] stones) {
-        int n = stones.length;
-        boolean[] visited = new boolean[n];
         
-        int group = 0;
+        int[] parent = new int[20005];
+        int[] rank = new int[20005];
         
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                dfs(i, stones, visited);
-                group++;
-            }
+        Arrays.fill(parent, -1);
+        
+        for (int[] stone : stones) {
+            union(stone[0], stone[1] + 10001, parent, rank);
         }
-        return (n - group);
+        
+        Set<Integer> set = new HashSet<>();
+        for (int[] stone : stones) {
+            set.add(find(stone[0], parent));
+        }
+        
+        return stones.length - set.size();
     }
 }
