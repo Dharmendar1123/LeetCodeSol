ae8c427d942e3b460108a4fe751fb6354e23334f
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-15 10:21:37 +0530

    Time: 381 ms (12.09%), Space: 44.8 MB (13.73%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        int n = nums.length;
+        int maxEl = Arrays.stream(nums).max().getAsInt();
+        int[] vec = new int[maxEl + 1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int d = Math.abs(nums[i] - nums[j]);
+                vec[d]++;
+            }
+        }
+
+        for (int d = 0; d <= maxEl; d++) {
+            k -= vec[d];
+            if (k <= 0) {
+                return d;
+            }
+        }
+        return -1;
+    }
+}
