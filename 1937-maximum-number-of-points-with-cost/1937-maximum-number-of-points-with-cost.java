21e1b352b14e93fa57571e3d3ec144ffc40fd605
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-17 23:06:34 +0530

    Time: 8 ms (68.82%), Space: 83.2 MB (53.53%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
@@ -1,39 +1,28 @@
 class Solution {
     public long maxPoints(int[][] points) {
-        int m = points.length;
-        int n = points[0].length;
-        
-        long[] prev = new long[n];
-        
-        for (int col = 0; col < n; ++col) {
-            prev[col] = points[0][col];
+        int cols = points[0].length;
+        long[] currRow = new long[cols];
+        long[] prevRow = new long[cols];
+
+        for (int[] row : points) {
+            long runningMax = 0;
+
+            for (int col = 0; col < cols; ++col) {
+                runningMax = Math.max(runningMax - 1, prevRow[col]);
+                currRow[col] = runningMax;
+            }
+
+            runningMax = 0;
+            for (int col = cols - 1; col >= 0; --col) {
+                runningMax = Math.max(runningMax - 1, prevRow[col]);
+                currRow[col] = Math.max(currRow[col], runningMax) + row[col];
+            }
+            prevRow = currRow;
         }
-        
-        for (int i = 1; i < m; ++i) {
-            long[] curr = new long[n];
-            long[] left = new long[n];
-            long[] right = new long[n];
-            
-            left[0] = prev[0];
-            for (int j = 1; j < n; ++j) {
-                left[j] = Math.max(prev[j], left[j - 1] - 1);
-            }
-            
-            right[n-1] = prev[n-1];
-            for (int j = n - 2; j >= 0; --j) {
-                right[j] = Math.max(prev[j], right[j + 1] - 1);
-            }
-            
-            for (int j = 0; j < n; ++j) {
-                curr[j] = points[i][j] + Math.max(left[j], right[j]);
-            }
-            
-            prev = curr;
-        }
-        
+
         long max = Long.MIN_VALUE;
-        for (long val : prev) {
-            max = Math.max(max, val);
+        for (int col = 0; col < cols; ++col) {
+            max = Math.max(max, prevRow[col]);
         }
         return max;
     }
