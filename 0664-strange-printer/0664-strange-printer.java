fe43ef0c5069f02a1f5fd693921c72d60795fc9b
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-21 10:58:14 +0530

    Time: 16 ms (52.65%), Space: 43.8 MB (11.43%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.java
@@ -0,0 +1,53 @@
+class Solution {
+    int n;
+    int[][] dp;
+    
+    private int solve(int l, int r, String s) {
+        
+        if (l == r) {
+            return 1;
+        }
+        
+        if (l > r) {
+            return 0;
+        }
+        
+        if (dp[l][r] != -1) {
+            return dp[l][r];
+        }
+        
+        int i = l + 1;
+        while (i <= r && s.charAt(i) == s.charAt(l)) {
+            i++;
+        }
+        
+        if (i == r + 1) {
+            return 1;
+        }
+        
+        int basic = 1 + solve(i, r, s);
+        
+        int optimize = Integer.MAX_VALUE;
+        
+        for (int j = i; j <= r; ++j) {
+            if (s.charAt(j) == s.charAt(l)) {
+                int ans = solve(i, j - 1, s) + solve(j, r, s);
+                
+                optimize = Math.min(optimize, ans);
+            }
+        }
+        
+        return dp[l][r] = Math.min(basic, optimize);
+    }
+    
+    public int strangePrinter(String s) {
+        n = s.length();
+        dp = new int[n+1][n+1];
+        
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        
+        return solve(0, n-1, s);
+    }
+}
