591b8aefc3e3c87ddd75be8bcf37c04056c8f4f3
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-22 16:48:45 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (51.69%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,10 +1,13 @@
 class Solution {
     public int findComplement(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        
         int numBits = (int)(Math.log(num) / Math.log(2)) + 1;
         
-        for (int i = 0; i < numBits; ++i) {
-            num = num ^ (1 << i);
-        }
-        return num;
+        int mask = (1 << numBits) - 1;
+        
+        return num ^ mask;
     }
 }
