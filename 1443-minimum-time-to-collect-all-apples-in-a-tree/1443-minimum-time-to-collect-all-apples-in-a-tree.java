a8286977033637962b65ef05817301dd7a7a0712
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-23 18:48:52 +0530

    Time: 15 ms (97.48%), Space: 76.4 MB (27.82%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,14 +1,14 @@
 class Solution {
     
-    private int dfs(int curr, int parent, Map<Integer, List<Integer>> adj, List<Boolean> hasApple) {
+    private int dfs(int curr, int parent, List<Integer>[] adj1, List<Boolean> hasApple) {
         int time = 0;
         
-        for (int child : adj.getOrDefault(curr, new ArrayList<>())) {
+        for (int child : adj1[curr]) {
             if (child == parent) {
                 continue;
             }
             
-            int timeFromChild = dfs(child, curr, adj, hasApple);
+            int timeFromChild = dfs(child, curr, adj1, hasApple);
             
             if (timeFromChild > 0 || hasApple.get(child)) {
                 time += timeFromChild + 2;
@@ -19,16 +19,24 @@ class Solution {
     
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+        // Map<Integer, List<Integer>> adj = new HashMap<>();
+        List<Integer>[] adj1 = new ArrayList[n+1];
+        
+        for (int i = 0; i < n; ++i) {
+            adj1[i] = new ArrayList<>();
+        }
         
         for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
             
-            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-            adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+            // adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+            // adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+            
+            adj1[u].add(v);
+            adj1[v].add(u);
         }
         
-        return dfs(0, -1, adj, hasApple);
+        return dfs(0, -1, adj1, hasApple);
     }
 }
