cae0d45dc9c5af178e8412bef374799ecd1c3f09
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-28 00:11:28 +0530

    Time: 24 ms (82.88%), Space: 120.5 MB (36.23%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,6 +1,26 @@
 class Solution {
+    int ans;
+    
+    private void dfs(int u, List<int[]>[] adj, boolean[] visited) {
+        
+        visited[u] = true;
+        
+        for (int[] v : adj[u]) {
+            int node = v[0];
+            int dist = v[1];
+            
+            ans = Math.min(ans, dist);
+            
+            if (!visited[node]) {
+                dfs(node, adj, visited);
+            }
+        }
+    }
+    
     public int minScore(int n, int[][] roads) {
+        
         List<int[]>[] adj = new ArrayList[n+1];
+        
         for (int i = 1; i <= n; ++i) {
             adj[i] = new ArrayList<>();
         }
@@ -14,31 +34,12 @@ class Solution {
             adj[v].add(new int[] {u, distance});
         }
         
-        int ans = Integer.MAX_VALUE;
-        
-        Queue<int[]> que = new LinkedList<>();
-        que.offer(new int[] {1, Integer.MAX_VALUE});
+        ans = Integer.MAX_VALUE;
         
         boolean[] visited = new boolean[n+1];
-        visited[1] = true;
         
-        while (!que.isEmpty()) {
-            int[] top = que.poll();
-            int u = top[0];
-            int dist = top[1];
-                
-            for (int[] v : adj[u]) {
-                int node = v[0];
-                int d = v[1];
-                
-                ans = Math.min(ans, d);
-                
-                if (!visited[node]) {
-                    que.offer(new int[] {node, d});
-                    visited[node] = true;
-                }
-            }
-        }
+        dfs(1, adj, visited);
+        
         return ans;
     }
 }
