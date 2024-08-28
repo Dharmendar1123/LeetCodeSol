22b2183f54e9a4cca273da2ed2ba9736853257d1
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-28 11:38:46 +0530

    Time: 27 ms (38.06%), Space: 74.9 MB (86.01%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,23 +1,37 @@
 class Solution {
     
+    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
     private boolean isSubIsland(int[][] grid1, int[][] grid2, int i, int j) {
-        if (i < 0 || i >= grid1.length || j < 0 || j >= grid1[0].length) {
-            return true;
-        }
+        int m = grid1.length;
+        int n = grid1[0].length;
         
-        if (grid2[i][j] != 1) {
-            return true;
-        }
+        boolean result = true;
         
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[] {i, j});
         grid2[i][j] = -1;
         
-        boolean result = (grid1[i][j] == 1);
-        
-        result = result & isSubIsland(grid1, grid2, i-1, j);
-        result = result & isSubIsland(grid1, grid2, i+1, j);
-        result = result & isSubIsland(grid1, grid2, i, j-1);
-        result = result & isSubIsland(grid1, grid2, i, j+1);
-        
+        while (!que.isEmpty()) {
+            int[] top = que.poll();
+            
+            int x = top[0];
+            int y = top[1];
+            
+            if (grid1[x][y] != 1) {
+                result = false;
+            }
+            
+            for (int[] dir : directions) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+                
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid2[newX][newY] == 1) {
+                    grid2[newX][newY] = -1;
+                    que.offer(new int[] {newX, newY});
+                }
+            }
+        }
         return result;
     }
     
