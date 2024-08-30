95187dda10af1ec3ee321a3356dc76a0bbd407c8
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-30 19:47:20 +0530

    Time: 212 ms (65.56%), Space: 49.8 MB (88.89%) - LeetHub

--- 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.java
+++ 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.java
@@ -1,83 +1,80 @@
 class Solution {
-    
-    private static final int LARGE_VALUE = (int) 2e9;
-    
-    private long dijkstraAlgo(int[][] edges, int n, int src, int dst) {
-        List<long[]>[] adj = new ArrayList[n];
-        
-        for (int i = 0; i < n; ++i) {
-            adj[i] = new ArrayList<>();
-        }
-        
-        for (int[] edge : edges) {
-            if (edge[2] != -1) {
-                int u = edge[0];
-                int v = edge[1];
-                int wt = edge[2];
-                
-                adj[u].add(new long[] {v, wt});
-                adj[v].add(new long[] {u, wt});
-            }
-        }
-        
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
-        long[] result = new long[n];
-        boolean[] visited = new boolean[n];
-        Arrays.fill(result, Long.MAX_VALUE);
-        result[src] = 0;
-        pq.offer(new long[] {0, src});
-        
-        while(!pq.isEmpty()) {
-            long[] curr = pq.poll();
-            long currDist = curr[0];
-            int currNode = (int) curr[1];
-            
-            if (visited[currNode]) {
-                continue;
-            }
-            visited[currNode] = true;
-            
-            for (long[] neighbor : adj[currNode]) {
-                int nbr = (int) neighbor[0];
-                long weight = neighbor[1];
-                
-                if (result[nbr] > currDist + weight) {
-                    result[nbr] = currDist + weight;
-                    pq.offer(new long[] {result[nbr], nbr});
-                }
-            }
-        }
-        return result[dst];
-    }
-    
+
+    List<int[]>[] graph;
+    private static final int INF = (int) 2e9;
+
     public int[][] modifiedGraphEdges(int n, int[][] edges, int source, int destination, int target) {
         
-        long currentShortest = dijkstraAlgo(edges, n, source, destination);
-        
-        if (currentShortest < target) {
-            return new int[0][];
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
+
+        for (int[] edge : edges) {
+            if (edge[2] != -1) {
+                graph[edge[0]].add(new int[] { edge[1], edge[2] });
+                graph[edge[1]].add(new int[] { edge[0], edge[2] });
+            }
+        }
+
         
-        boolean matchedTarget = (currentShortest == target);
+        int currentShortestDistance = runDijkstra(n, source, destination);
+        if (currentShortestDistance < target) {
+            return new int[0][0];
+        }
+
+        boolean matchesTarget = (currentShortestDistance == target);
+
         
         for (int[] edge : edges) {
-            if (edge[2] == -1) {
-                edge[2] = matchedTarget ? LARGE_VALUE : 1;
+            if (edge[2] != -1) continue; 
+
+            edge[2] = matchesTarget ? INF : 1;
+            graph[edge[0]].add(new int[] { edge[1], edge[2] });
+            graph[edge[1]].add(new int[] { edge[0], edge[2] });
+
+            if (!matchesTarget) {
                 
-                if (!matchedTarget) {
-                    long newShortest = dijkstraAlgo(edges, n, source, destination);
-                    
-                    if (newShortest <= target) {
-                        matchedTarget = true;
-                        edge[2] += (target - newShortest);
-                    }
+                int newDistance = runDijkstra(n, source, destination);
+                
+                if (newDistance <= target) {
+                    matchesTarget = true;
+                    edge[2] += target - newDistance;
                 }
             }
         }
+
+        return matchesTarget ? edges : new int[0][0];
+    }
+
+    private int runDijkstra(int n, int source, int destination) {
         
-        if (!matchedTarget) {
-            return new int[0][];
+        int[] minDistance = new int[n];
+        boolean[] visited = new boolean[n];
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+        Arrays.fill(minDistance, INF);
+        minDistance[source] = 0;
+        minHeap.add(new int[] { source, 0 });
+
+        while (!minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int u = curr[0];
+            int d = curr[1];
+
+            if (d > minDistance[u]) continue;
+
+            for (int[] neighbor : graph[u]) {
+                int v = neighbor[0];
+                int weight = neighbor[1];
+
+                if (d + weight < minDistance[v]) {
+                    minDistance[v] = d + weight;
+                    minHeap.add(new int[] { v, minDistance[v] });
+                }
+            }
         }
-        return edges;
+
+        return minDistance[destination];
     }
 }
