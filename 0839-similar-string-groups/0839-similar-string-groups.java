b4675ea820e6ed5d0909b9a6c77d7718d3882fb4
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-31 00:23:27 +0530

    Time: 97 ms (42.82%), Space: 43.3 MB (37.23%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -0,0 +1,55 @@
+class Solution {
+    
+    private void dfs(int u, List<Integer>[] adj, boolean[] visited) {
+        visited[u] = true;
+        
+        for (int v : adj[u]) {
+            if (!visited[v]) {
+                dfs(v, adj, visited);
+            }
+        }
+    }
+    
+    private boolean isSimilar(String s1, String s2) {
+        int diff = 0;
+        
+        int n = s1.length();
+        
+        for (int i = 0; i < n; ++i) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+            }
+        }
+        return diff == 2 || diff == 0;
+    }
+    
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        
+        List<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; ++i) {
+            adj[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                
+                if (isSimilar(strs[i], strs[j])) {
+                    adj[i].add(j);
+                    adj[j].add(i);
+                }
+            }
+        }
+        
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                dfs(i, adj, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+}
