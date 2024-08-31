78ea65dd366e9b06a941137776919db925566c6d
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-08-31 10:42:44 +0530

    Time: 10 ms (92.38%), Space: 56.6 MB (62.79%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,49 +1,31 @@
 class Solution {
-    
-    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-        List<Pair<Integer, Double>>[] adj = new ArrayList[n+1];
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        double[] dist = new double[n];
+        Arrays.fill(dist, 0.0);
+        dist[start_node] = 1.0;
         
-        for (int i = 0; i < n; ++i) {
-            adj[i] = new ArrayList<>();
-        }
         
-        int i = 0;
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            double wt = succProb[i];
-            
-            adj[u].add(new Pair<>(v, wt));
-            adj[v].add(new Pair<>(u, wt));
-            i++;
-        }
-        
-        PriorityQueue<Pair<Double, Integer>> pq = new PriorityQueue<>((a, b) -> -Double.compare(a.getKey(), b.getKey()));
-        double[] result = new double[n];
-        
-        result[start] = 1d;
-        pq.offer(new Pair<>(1.0, start));
-        
-        while (!pq.isEmpty()) {
-            Pair<Double, Integer> curr = pq.poll();
-            double currProb = curr.getKey();
-            int currNode = curr.getValue();
-            
-            if (currNode == end) {
-                return currProb;
-            }
-            
-            for (Pair<Integer, Double> next : adj[currNode]) {
-                int nextNode = next.getKey();
-                double pathProb = next.getValue();
+        for (int i = 0; i < n - 1; i++) {
+            boolean updated = false;
+            for (int j = 0; j < edges.length; j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
                 
-                if (currProb * pathProb > result[nextNode]) {
-                    result[nextNode] = currProb * pathProb;
-                    pq.offer(new Pair<>(result[nextNode], nextNode));
+                if (dist[u] * prob > dist[v]) {
+                    dist[v] = dist[u] * prob;
+                    updated = true;
+                }
+                
+                if (dist[v] * prob > dist[u]) {
+                    dist[u] = dist[v] * prob;
+                    updated = true;
                 }
             }
+            
+            if (!updated) break;
         }
         
-        return 0d;
+        return dist[end_node];
     }
 }
