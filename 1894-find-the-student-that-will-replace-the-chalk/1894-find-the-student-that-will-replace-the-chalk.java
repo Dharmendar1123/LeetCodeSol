2e7a710e75bb4eb2982bd9e4a2056cefcf4ee9c6
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-09-02 20:25:21 +0530

    Time: 2 ms (72.59%), Space: 60.5 MB (8.89%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -2,13 +2,20 @@ class Solution {
     public int chalkReplacer(int[] chalk, int k) {
         int n = chalk.length;
         
-        while (true) {
-            for (int i = 0; i < n; ++i) {
-                if (k < chalk[i]) {
-                    return i;
-                }
-                k -= chalk[i];
-            }
+        long sum = 0;
+        for (int val : chalk) {
+            sum += val;
         }
+        
+        int idx = 0;
+        k = (int)(k % sum);
+        
+        for (int i = 0; i < n; ++i) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k -= chalk[i];
+        }
+        return -1;
     }
 }
