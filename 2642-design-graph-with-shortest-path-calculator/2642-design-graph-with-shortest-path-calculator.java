34a1b6aaad988e91050f0ad79a6e9a79fe97509f
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-09-17 18:14:33 +0530

    Time: 70 ms (86.84%), Space: 55 MB (90.98%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
@@ -1,58 +1,47 @@
 class Graph {
-    List<int[]>[] adj;
+    int[][] adj;
     int N;
-    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
     public Graph(int n, int[][] edges) {
         N = n;
-        adj = new ArrayList[n];
-        for (int i = 0; i < N; ++i) {
-            adj[i] = new ArrayList<>();
+        adj = new int[N][N];
+        
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(adj[i], (int)1e9);
+            adj[i][i] = 0;
         }
         
         for (int[] edge : edges) {
-            
             int u = edge[0];
             int v = edge[1];
             int cost = edge[2];
             
-            adj[u].add(new int[] {v, cost});
+            adj[u][v] = cost;
+        }
+        
+        for (int via = 0; via < n; ++via) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    adj[i][j] = Math.min(adj[i][j], adj[i][via] + adj[via][j]);
+                }
+            }
         }
     }
     
     public void addEdge(int[] edge) {
-        
         int u = edge[0];
         int v = edge[1];
         int cost = edge[2];
-            
-        adj[u].add(new int[] {v, cost});
+        
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                adj[i][j] = Math.min(adj[i][j], adj[i][u] + cost + adj[v][j]);
+            }
+        }
     }
     
     public int shortestPath(int node1, int node2) {
-        
-        int[] result = new int[N];
-        Arrays.fill(result, Integer.MAX_VALUE);
-        
-        result[node1] = 0;
-        pq.offer(new int[] {0, node1});
-        
-        while (!pq.isEmpty()) {
-            int[] top = pq.poll();
-            int node = top[1];
-            int dist = top[0];
-            
-            for (int[] v : adj[node]) {
-                int d = v[1];
-                int adjNode = v[0];
-                
-                if (d + dist < result[adjNode]) {
-                    result[adjNode] = d + dist;
-                    pq.offer(new int[] {d + dist, adjNode});
-                }
-            }
-        }
-        return result[node2] == Integer.MAX_VALUE ? -1 : result[node2];
+        return adj[node1][node2] == (int)1e9 ? -1 : adj[node1][node2];
     }
 }
 
