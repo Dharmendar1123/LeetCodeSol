6bc66db64bccbe0b22ac1df25b2d9efb54bfed86
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-09-19 19:49:08 +0530

    Time: 17 ms (59.89%), Space: 45.6 MB (71.32%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -1,35 +1,50 @@
 class Solution {
-
-    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        long[][] distances = new long[26][26];
-        for (int i = 0; i < 26; i++) {
-            Arrays.fill(distances[i], Integer.MAX_VALUE);
-        }
-
+    private void FloydWarshall(long[][] distances, char[] original, char[] changed, int[] cost, int size) {
         for (int i = 0; i < original.length; ++i) {
             int s = original[i] - 'a';
             int t = changed[i] - 'a';
             distances[s][t] = Math.min(distances[s][t], (long) cost[i]);
         }
 
-        for (int k = 0; k < 26; ++k) {
-            for (int i = 0; i < 26; ++i) {
-                for (int j = 0; j < 26; ++j) {
+        for (int k = 0; k < size; ++k) {
+            for (int i = 0; i < size; ++i) {
+                for (int j = 0; j < size; ++j) {
                     distances[i][j] = Math.min(distances[i][j], distances[i][k] + distances[k][j]);
                 }
             }
         }
+    }
+
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        
+        int size = 0;
+        for (int i = 0; i < cost.length; i++) {
+            size = Math.max(Math.max(size, changed[i] - 'a'), original[i] - 'a');
+        }
+
+        size++;
+        
+        long[][] distances = new long[size][size];
+        for (int i = 0; i < size; i++) {
+            Arrays.fill(distances[i], Integer.MAX_VALUE);
+            distances[i][i] = 0;
+        }
+
+        FloydWarshall(distances, original, changed, cost, size);
 
         long ans = 0;
         for (int i = 0; i < source.length(); i++) {
+            int ch1 = source.charAt(i) - 'a';
+            int ch2 = target.charAt(i) - 'a';
+            
             if (source.charAt(i) == target.charAt(i)) {
                 continue;
             }
 
-            if (distances[source.charAt(i) - 'a'][target.charAt(i) - 'a'] == Integer.MAX_VALUE) {
+            if (ch1 >= size || ch2 >= size || distances[ch1][ch2] == Integer.MAX_VALUE) {
                 return -1;
             } else {
-                ans += distances[source.charAt(i) - 'a'][target.charAt(i) - 'a'];
+                ans += distances[ch1][ch2];
             }
         }
 
