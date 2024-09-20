4b774ff74b1d78591ea5e826a6353f63017a3dc7
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-09-20 18:18:32 +0530

    Time: 8 ms (63.77%), Space: 44.8 MB (46.05%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,12 +1,39 @@
 class Solution {
-    public String shortestPalindrome(String s) {
-        String reverse = new StringBuilder(s).reverse().toString();
-        
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.substring(0, s.length() - i).equals(reverse.substring(i))) {
-                return reverse.substring(0, i) + s;
+    public void computeLPS(String pattern, int[] lps) {
+        int M = pattern.length();
+        int len = 0;
+
+        lps[0] = 0;
+
+        int i = 1;
+        while (i < M) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return reverse + s;
+    }
+
+    public String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString(); 
+
+        String temp = s + "-" + rev;
+
+        int[] LPS = new int[temp.length()]; 
+        computeLPS(temp, LPS); 
+
+        int longestLPSLength = LPS[temp.length() - 1]; 
+
+        String culprit = rev.substring(0, s.length() - longestLPSLength);
+
+        return culprit + s;
     }
 }
