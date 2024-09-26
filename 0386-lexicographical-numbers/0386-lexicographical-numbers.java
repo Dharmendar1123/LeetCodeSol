930952b35160a2b738a27c6fd024953b9f8f1373
Parmar Dharmendar <64119309+Dharmendar1123@users.noreply.github.com>
2024-09-26 17:11:54 +0530

    Time: 2 ms (76.45%), Space: 48.9 MB (12.31%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,18 +1,26 @@
 class Solution {
+    private void addToNumbers(int currNum, int n, List<Integer> numbers) {
+        if (currNum > n) {
+            return;
+        }
+        numbers.add(currNum);
+        
+        for (int i = 0; i <= 9; i++) {
+            int newNum = (currNum * 10) + i;
+            
+            if (newNum > n) {
+                return;
+            }
+            addToNumbers(newNum, n, numbers);
+        }
+    }
+    
     public List<Integer> lexicalOrder(int n) {
         List<Integer> numbers = new ArrayList<>(n);
+        
         for (int i = 1; i <= Math.min(9, n); i++) {
             addToNumbers(i, n, numbers);
         }
         return numbers;
-    }
-
-    private void addToNumbers(int number, int maxNumber, List<Integer> numbers) {
-        if (number <= maxNumber) {
-            numbers.add(number);
-            for (int i = 0; i <= 9; i++) {
-                addToNumbers(number * 10 + i, maxNumber, numbers);
-            }
-        }
     }
 }
